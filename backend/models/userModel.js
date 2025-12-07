416013cb5c16271f3296aa9ec4c9626a94fb8eaa
Sanchitbnsal <bsanchit40@gmail.com>
2025-12-07 13:58:11 +0530

    schema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,6 +4,11 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    phone: { type: String },
+    address: {
+        line1: { type: String, trim: true },
+        line2: { type: String, trim: true },
+    },
 })
 
 const userModel = mongoose.models.user || mongoose.model("user", userSchema);
